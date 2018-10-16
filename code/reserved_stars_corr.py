e3da108abdb6b0f42798a2bec62bd1c87f140a02
andalenavals <aanavarroa@unal.edu.co>
2018-10-16 12:33:51 -0300

    adding script to run in NERSC limiting to riz band in the reading including exposure file

--- code/reserved_stars_corr.py
+++ code/reserved_stars_corr.py
@@ -11,7 +11,7 @@ def parse_args():
                         default='/home/dfa/sobreira/alsina/DESWL/psf/ally3.grizY',
                         #default='/home/dfa/sobreira/alsina/DESWL/psf/testexp', 
                         help='list of exposures (in lieu of separate exps)')
-    parser.add_argument('--bands', default='grizY', type=str,
+    parser.add_argument('--bands', default='riz', type=str,
                          help='Limit to the given bands')
     parser.add_argument('--bandcombo', default=False,
                         action='store_const', const=True,
